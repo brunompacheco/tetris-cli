1faaa3b59c65fec9b65454747d7750638a25a69a
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2022-03-30 14:57:02 -0300

    moving tetrimino in the well

--- src/tetris/main.py
+++ src/tetris/main.py
@@ -1,22 +1,60 @@
 import sys
-from time import sleep
 
 import click
 
 from . import __version__
 
-from asciimatics.effects import Print
-from asciimatics.exceptions import ResizeScreenError
+from asciimatics.effects import Print, Sprite
+from asciimatics.event import KeyboardEvent
+from asciimatics.exceptions import ResizeScreenError, StopApplication
 from asciimatics.renderers import StaticRenderer
+from asciimatics.paths import DynamicPath
 from asciimatics.scene import Scene
 from asciimatics.screen import ManagedScreen, Screen
 
 
+class KeyboardController(DynamicPath):
+    def process_event(self, event):
+        if isinstance(event, KeyboardEvent):
+            key = event.key_code
+            if key == Screen.KEY_UP:
+                self._y -= 1
+                self._y = max(self._y, 2)
+            elif key == Screen.KEY_DOWN:
+                self._y += 1
+                self._y = min(self._y, self._screen.height-2)
+            elif key == Screen.KEY_LEFT:
+                self._x -= 2
+                self._x = max(self._x, 3)
+            elif key == Screen.KEY_RIGHT:
+                self._x += 2
+                self._x = min(self._x, self._screen.width-3)
+            else:
+                return event
+        else:
+            return event
+
+class Tetrimino(Sprite):
+    def __init__(self, screen, **kwargs):
+        box = "####\n"
+        box += "####"
+
+        super().__init__(
+            screen,
+            renderer_dict={
+                "default": StaticRenderer(images=[box]),
+            },
+            path=KeyboardController(screen, 1 + screen.width // 2, screen.height // 2),
+            clear=True,
+            **kwargs
+        )
+
 class Well(Print):
     def __init__(self, screen, nrows=20, ncols=10) -> None:
+        bg_dot = "  "
         board = ""
         board += "${7,2,7}_" * (2 * ncols + 4) + '\n'
-        board += ("${7,2,7}__${7,2,0}" + " ." * ncols + '${7,2,7}__\n') * nrows
+        board += ("${7,2,7}__${7,2,0}" + bg_dot * ncols + '${7,2,7}__\n') * nrows
         board += "${7,2,7}_" * (2 * ncols + 4) + '\n'
 
         super().__init__(
@@ -25,14 +63,22 @@ class Well(Print):
             x=(screen.width // 2) - (ncols // 2),
             y=(screen.height // 2) - (nrows // 2),
             speed=0,
+            transparent=True,
         )
 
 @ManagedScreen
 def demo(screen: Screen = None):
     effects = [
         Well(screen),
+        Tetrimino(screen),
     ]
-    screen.play([Scene(effects, -1),])
+    # screen.play([Scene(effects, -1),])
+    screen.set_scenes([Scene(effects, -1),])
+    while True:
+        try:
+            screen.draw_next_frame()
+        except StopApplication:
+            break
 
 @click.command()
 @click.version_option(version=__version__)
