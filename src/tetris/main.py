29d488440c65a501546ebcdb1b3680d2f0ca1d7d
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2022-03-30 18:16:56 -0300

    refactor rendering to main; drop tetromino using threads

--- src/tetris/main.py
+++ src/tetris/main.py
@@ -1,19 +1,17 @@
 import sys
+from threading import Timer
 from time import sleep
 
 import click
-from tetris.blocks import TetrominoI
-
-from tetris.well import Well
+import numpy as np
 
 from . import __version__
+from tetris.blocks import TetrominoI
+from tetris.well import Well
 
-from asciimatics.effects import Print, Sprite
 from asciimatics.event import KeyboardEvent
 from asciimatics.exceptions import ResizeScreenError, StopApplication
-from asciimatics.renderers import StaticRenderer
 from asciimatics.paths import DynamicPath
-from asciimatics.scene import Scene
 from asciimatics.screen import ManagedScreen, Screen
 
 
@@ -38,30 +36,81 @@ class KeyboardController(DynamicPath):
         else:
             return event
 
+def draw_well(matrix: np.ndarray, bg_square: str = " .") -> str:
+    matrix = np.where(matrix == 0, bg_square, 2 * chr(0x2588))
+
+    lines = [''.join(line) for line in matrix]
+    board = '\n'.join(lines)
+
+    return board
+
+def update_screen(screen, well, tetromino):
+    # TODO: draw only changes, avoid flickering
+
+    x = (screen.width // 2) - (well.ncols // 2)
+    y = (screen.height // 2) - (well.nrows // 2)
+
+    well_str = draw_well(well.add_tetromino(tetromino))
+    for l, line in enumerate(well_str.split('\n')):
+        screen.print_at(
+            line,
+            x=x,
+            y=y+l
+        )
+
+    screen.refresh()
+
+class TetrominoDropper():
+    def __init__(self) -> None:
+        self.timer = None
+
+        self._stop = False
+
+    @property
+    def is_alive(self):
+        if self.timer is None:
+            return False
+        else:
+            return self.timer.is_alive()
+
+    def run(self, interval: float, tetromino):
+        self.timer = Timer(interval, self.run, (interval, tetromino))
+        self.timer.daemon = True
+        self.timer.start()
+
+        tetromino.y += 1
+
+        # TODO: check for collision
+        # if collision:  # in the future update
+        #     tetromino.y -= 1
+        #     self.timer.cancel()
+        # maybe raise event on collision?
+
 @ManagedScreen
 def play(screen: Screen = None):
     well = Well()
 
+    ## MAIN LOOP
+
+    # 1. generate new tetromino
     t_I = TetrominoI(well)
-    t_I.y = 5
-    well.add_tetromino(t_I)
+
+    # 2. drop tetromino until it touches the others
+    dropper = TetrominoDropper()
+    dropper.run(1, t_I)
 
     while True:
         ev = screen.get_key()
         if ev in (ord('Q'), ord('q')):
             return
 
-        sleep(1)
-        # screen.clear()
-        x = (screen.width // 2) - (well.ncols // 2)
-        y = (screen.height // 2) - (well.nrows // 2)
-        for l, line in enumerate(str(well).split('\n')):
-            screen.print_at(
-                line,
-                x=x,
-                y=y+l
-            )
-        screen.refresh()
+        update_screen(screen, well, t_I)
+
+    # 3. add tetromino to well
+
+    # 4. check for line clears
+
+
 
 @click.command()
 @click.version_option(version=__version__)
--- src/tetris/well.py
+++ src/tetris/well.py
@@ -2,25 +2,23 @@ import numpy as np
 
 
 class Well():
-    def __init__(self, nrows=20, ncols=10, bg_square=" .") -> None:
+    def __init__(self, nrows=20, ncols=10) -> None:
         self.nrows = nrows
         self.ncols = ncols
 
-        self.bg_square = bg_square
-
         self.matrix = np.zeros((nrows, ncols))
 
-    def add_tetromino(self, tetromino):
+    def add_tetromino(self, tetromino, in_place=False):
         t_matrix = tetromino.matrix
 
         x = tetromino.x
         y = tetromino.y
-        self.matrix[y:y + t_matrix.shape[0], x:x+t_matrix.shape[1]] += t_matrix
 
-    def __str__(self) -> str:
-        matrix = np.where(self.matrix == 0, self.bg_square, 2 * chr(0x2588))
+        new_matrix = self.matrix.copy()
 
-        lines = [''.join(line) for line in matrix]
-        board = '\n'.join(lines)
+        new_matrix[y:y + t_matrix.shape[0], x:x+t_matrix.shape[1]] += t_matrix
 
-        return board
+        if in_place:
+            self.matrix = new_matrix
+        else:
+            return new_matrix
